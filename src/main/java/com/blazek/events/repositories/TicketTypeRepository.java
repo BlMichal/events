57b29d0ec3a91c512aac33e0f072bc01870c1549
Michal Blažek <blaza0@email.cz>
2025-07-08 16:25:14 +0200

    Ticket type service and controller implementation

--- src/main/java/com/blazek/events/repositories/TicketTypeRepository.java
+++ src/main/java/com/blazek/events/repositories/TicketTypeRepository.java
@@ -1,11 +1,22 @@
 package com.blazek.events.repositories;
 
 import com.blazek.events.domain.entities.TicketType;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TicketTypeRepository extends JpaRepository<TicketType, UUID> {
+
+    // Locking row to prevent race conditions (other transaction don't affect current record)
+    @Query("SELECT tt FROM TicketType tt WHERE tt.id = :id")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<TicketType> findByIdWithLock(@Param("id") UUID id);
+
 }
--- src/main/java/com/blazek/events/services/TicketTypeServiceImpl.java
+++ src/main/java/com/blazek/events/services/TicketTypeServiceImpl.java
@@ -1,8 +1,10 @@
 package com.blazek.events.services;
 
 import com.blazek.events.domain.entities.Ticket;
+import com.blazek.events.domain.entities.TicketStatusEnum;
 import com.blazek.events.domain.entities.TicketType;
 import com.blazek.events.domain.entities.User;
+import com.blazek.events.exceptions.TicketSoldOutException;
 import com.blazek.events.exceptions.TicketTypeNotFoundException;
 import com.blazek.events.exceptions.UserNotFoundException;
 import com.blazek.events.repositories.QrCodeRepository;
@@ -22,16 +24,37 @@ public class TicketTypeServiceImpl implements TicketTypeService {
     private final UserRepository userRepository;
     private final TicketRepository ticketRepository;
     private final TicketTypeRepository ticketTypeRepository;
-    private final QrCodeRepository qrCodeRepository;
+    private final QrCodeService qrCodeService;
 
     @Override
     @Transactional
     public Ticket purchaseTicket(UUID userId, UUID ticketTypeId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(String.format("User with ID '%s' was not found", userId)));
-        TicketType ticketType = ticketTypeRepository.findById(ticketTypeId).orElseThrow(() -> new TicketTypeNotFoundException(String.format("Ticket type with ID '%s' was not found", ticketTypeId)));
+        // Get user by ID
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(String.format("User with ID '%s' was not found", userId)));
 
-        ticketRepository.countByTicketTypeId(ticketType.getId());
+        // Get ticket type with lock
+        TicketType ticketType = ticketTypeRepository.findByIdWithLock(ticketTypeId)
+                .orElseThrow(() -> new TicketTypeNotFoundException(String.format("Ticket type with ID '%s' was not found", ticketTypeId)));
 
-        return null;
+        // Total ticket sold count by Ticket ID
+        int purchasedTickets = ticketRepository.countByTicketTypeId(ticketType.getId());
+
+        // Total ticket available
+        Integer totalAvailable = ticketType.getTotalAvailable();
+
+        if (purchasedTickets + 1 > totalAvailable) {
+            throw new TicketSoldOutException();
+        }
+
+        Ticket ticket = new Ticket();
+        ticket.setStatus(TicketStatusEnum.PURCHASED);
+        ticket.setTicketType(ticketType);
+        ticket.setPurchaser(user);
+
+        // Generate QRCODE with ticket data
+        qrCodeService.generateQrCode(ticket);
+
+        return ticketRepository.save(ticket);
     }
 }
